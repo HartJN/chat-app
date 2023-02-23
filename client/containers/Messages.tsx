5ddb971491c8f0266bfcbd8da68392faef14fc3f
HartJN <jameshart04@gmail.com>
2023-02-23 15:03:18 +1300

    feat: display live messages

---
+++ client/containers/Messages.tsx
@@ -0,0 +1,74 @@
+import EVENTS from 'config/events';
+import { useSockets } from 'context/socket.context';
+import { useEffect, useRef } from 'react';
+import styles from '@/styles/Messages.module.css';
+
+function MessagesContainer() {
+  const { socket, messages = [], roomId, username, setMessages } = useSockets();
+  const newMessageRef = useRef<HTMLTextAreaElement>(null);
+  const messageEndRef = useRef<HTMLDivElement | null>(null);
+
+  function handleSendMessage() {
+    const message = newMessageRef.current?.value;
+
+    if (!String(message).trim()) return;
+
+    socket.emit(EVENTS.CLIENT.SEND_ROOM_MESSAGE, { roomId, message, username });
+
+    const date = new Date();
+
+    setMessages([
+      ...messages,
+      {
+        username: 'You',
+        message,
+        time: `${date.getHours()}:${date.getMinutes()}`,
+      },
+    ]);
+
+    if (newMessageRef.current) {
+      newMessageRef.current.value = '';
+    }
+  }
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
+  if (!roomId)
+    return (
+      <div className={styles.messageBody}>Join a room to see messages</div>
+    );
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.messageList}>
+        {messages.map(({ message, username, time }, index) => {
+          return (
+            <div key={index} className={styles.message}>
+              <div key={index} className={styles.messageInner}>
+                <span className={styles.messageSender}>
+                  {username} - {time}
+                </span>
+                <span className={styles.messageBody}>{message}</span>
+              </div>
+            </div>
+          );
+        })}
+        <div ref={messageEndRef} />
+
+        <div />
+        <div className={styles.messageBox}>
+          <textarea
+            placeholder="Tell us what you are thinking"
+            rows={1}
+            ref={newMessageRef}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessagesContainer;
